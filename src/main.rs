d3b0ebbc1ad11b50fe89617cbcfee6e356d56977
Paolo Panarese <panarese.paolo@gmail.com>
2024-06-13 00:57:40 +0200

    updated alfred-rs version

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::LinkedList;
+use alfred_rs::connection::{Receiver, Sender};
 use alfred_rs::error::Error;
-use alfred_rs::connection::{Publisher, Subscriber};
+use alfred_rs::interface_module::InterfaceModule;
 use alfred_rs::message::{Message, MessageType};
-use alfred_rs::module::Module;
 use alfred_rs::log::warn;
 use alfred_rs::tokio;
 
@@ -12,27 +12,21 @@ const STT_REQUEST_TOPIC: &'static str = "audio_out";
 const TTS_REQUEST_TOPIC: &'static str = "tts";
 const AI_TOPIC: &'static str = "openai";
 
-async fn on_input(message: &mut Message, module: &mut Module) -> Result<(), Error> {
+async fn on_input(message: &mut Message, module: &mut InterfaceModule) -> Result<(), Error> {
     match message.message_type {
         MessageType::TEXT => {
             // TODO: manage errors
-            module.publish(AI_TOPIC.to_string(), message).await.expect("Error on publish");
+            module.send(AI_TOPIC.to_string(), message).await.expect("Error on publish");
         }
         MessageType::AUDIO => {
             let topic = message.response_topics.front().cloned().unwrap();
             message.response_topics = LinkedList::from(
                 [
                     AI_TOPIC.to_string(),
-                //    TTS_REQUEST_TOPIC.to_string(),
-                    topic
-                ]);
-            module.publish(STT_REQUEST_TOPIC.to_string(), message).await.expect("Error on publish");
+            module.send(STT_REQUEST_TOPIC.to_string(), message).await.expect("Error on publish");
         }
-        MessageType::PHOTO => {
-            warn!("Unable to analyse {:?} message type", MessageType::PHOTO);
-        }
-        MessageType::UNKNOWN => {
-            warn!("Unable to analyse {:?} message type", MessageType::UNKNOWN);
+        _ => {
+            warn!("Unable to analyse {:?} message type", message.message_type);
         }
     }
     Ok(())
@@ -41,10 +35,10 @@ async fn on_input(message: &mut Message, module: &mut Module) -> Result<(), Erro
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
-    let mut module = Module::new(MODULE_NAME.to_string()).await?;
-    module.subscribe(INPUT_TOPIC.to_string()).await.expect("Error on subscribe");
+    let mut module = InterfaceModule::new(MODULE_NAME.to_string()).await?;
+    module.listen(INPUT_TOPIC.to_string()).await.expect("Error on subscribe");
     loop {
-        let (topic, mut message) = module.get_message().await.expect("Error on getting new messages");
+        let (topic, mut message) = module.receive().await.expect("Error on getting new messages");
         match topic.as_str() {
             INPUT_TOPIC => on_input(&mut message, &mut module).await?,
             _ => {
