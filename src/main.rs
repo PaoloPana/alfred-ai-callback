6e9a7e6603442280904a94606b08d77358cf4624
Paolo Panarese <panarese.paolo@gmail.com>
2024-06-14 00:16:10 +0200

    updated audio flow

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use alfred_rs::error::Error;
 use alfred_rs::interface_module::InterfaceModule;
 use alfred_rs::message::{Message, MessageType};
 use alfred_rs::log::warn;
+use alfred_rs::pubsub_connection::REQUEST_TOPIC;
 use alfred_rs::tokio;
 
 const MODULE_NAME: &'static str = "ai_callback";
 const INPUT_TOPIC: &'static str = "ai_callback";
-const STT_REQUEST_TOPIC: &'static str = "audio_out";
+const STT_REQUEST_TOPIC: &'static str = "stt";
 const TTS_REQUEST_TOPIC: &'static str = "tts";
 const AI_TOPIC: &'static str = "openai";
 
@@ -16,13 +17,20 @@ async fn on_input(message: &mut Message, module: &mut InterfaceModule) -> Result
     match message.message_type {
         MessageType::TEXT => {
             // TODO: manage errors
-            module.send(AI_TOPIC.to_string(), message).await.expect("Error on publish");
+            let mut ai_message = message.clone();
+            ai_message.request_topic = "openai".to_string();
+            module.send(REQUEST_TOPIC.to_string(), &ai_message).await.expect("Error on publish");
         }
         MessageType::AUDIO => {
             let topic = message.response_topics.front().cloned().unwrap();
             message.response_topics = LinkedList::from(
                 [
+                    // TODO: setup in config.toml file
                     AI_TOPIC.to_string(),
+                    TTS_REQUEST_TOPIC.to_string(),
+                    topic
+                    //"audio_out".to_string()
+                ]);
             module.send(STT_REQUEST_TOPIC.to_string(), message).await.expect("Error on publish");
         }
         _ => {
