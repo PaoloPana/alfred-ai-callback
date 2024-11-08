15ba121e8406d6e1aeb99549ae2018e3edfaad62
Paolo Panarese <panarese.paolo@gmail.com>
2024-11-09 00:58:00 +0100

    updated alfred dev version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use alfred_rs::callback_module::CallbackModule;
+use alfred_rs::AlfredModule;
 use alfred_rs::connection::{Receiver, Sender};
 use alfred_rs::error::Error;
 use alfred_rs::message::Message;
@@ -8,7 +8,7 @@ use alfred_rs::tokio;
 const MODULE_NAME: &str = "ai_callback";
 const INPUT_TOPIC: &str = "ai_callback";
 
-async fn on_input(message: &Message, module: &mut CallbackModule) -> Result<(), Error> {
+async fn on_input(message: &Message, module: &mut AlfredModule) -> Result<(), Error> {
     let msg_text = message.text.as_str();
     let (relay_topic, relay_msg) = if msg_text.starts_with('`') && msg_text.ends_with('`') && msg_text.contains(": ") {
         let msg_text = msg_text.to_string();
@@ -31,7 +31,7 @@ async fn on_input(message: &Message, module: &mut CallbackModule) -> Result<(), 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
-    let mut module = CallbackModule::new(MODULE_NAME).await?;
+    let mut module = AlfredModule::new(MODULE_NAME).await?;
     module.listen(INPUT_TOPIC).await.expect("Error on subscribe");
     loop {
         let (topic, message) = module.receive().await.expect("Error on getting new messages");
